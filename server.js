7a7b609d180ce94155f82f247375bd698a99b34c
Fabien Berdah <fabien.berdah@gmail.com>
2021-06-13 23:34:24 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//This is going to be our main entry point to pour application
+
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+});

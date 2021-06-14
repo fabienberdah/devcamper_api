8b1d6dc1f9096d0bd36e1e0d23ec71b99bf04c7c
Fabien Berdah <fabien.berdah@gmail.com>
2021-06-14 22:33:56 +0300

    set up MongoDB and full CRUD for Bootcamp collection

--- server.js
+++ server.js
@@ -2,14 +2,43 @@
 
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to Database
+connectDB();
+
+//import Routes file
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+//BodyParser
+app.use(express.json());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps); //this will link the routes to the bootcamps file
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.green
+  );
+});
+
+//handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close Server and exit process
+  server.close(() => process.exit(1));
 });
